2d2deec5921a36eccb6e7107d89941b00e492c9c
bhaal275 <dominik.deren@windowslive.com>
2014-04-19 19:28:37 +0200

    Adding missing test types.

---
+++ AutoEquals/Tests/AutoEquals.Lib.Tests.Unit/TestObjects/BaseClass.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BaseClass.cs" company="AutoEquals">
+//   AutoEquals Library
+//   Licence: GNU GPL 2.0
+//   No warranty granted, use at your own risk.
+// </copyright>
+// <summary>
+//   The base class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AutoEquals.Lib.Tests.Unit.TestObjects
+{
+    /// <summary>
+    /// The base class.
+    /// </summary>
+    public class BaseClass : AutoEqualsBase
+    {
+        /// <summary>
+        /// Gets or sets the base property.
+        /// </summary>
+        public string BaseProperty { get; set; }
+    }
+}
---
+++ AutoEquals/Tests/AutoEquals.Lib.Tests.Unit/TestObjects/DerivedClass.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DerivedClass.cs" company="AutoEquals">
+//   AutoEquals Library
+//   Licence: GNU GPL 2.0
+//   No warranty granted, use at your own risk.
+// </copyright>
+// <summary>
+//   Defines the DerivedClass type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AutoEquals.Lib.Tests.Unit.TestObjects
+{
+    /// <summary>
+    /// The derived class.
+    /// </summary>
+    public class DerivedClass : BaseClass
+    {
+        /// <summary>
+        /// Gets or sets the derived property.
+        /// </summary>
+        public int DerivedProperty { get; set; }
+    }
+}
