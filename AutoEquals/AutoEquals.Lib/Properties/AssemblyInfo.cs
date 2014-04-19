d1f0626e485a398cde61791d9ff615e8bc5d30a1
bhaal275 <dominik.deren@windowslive.com>
2014-04-19 19:48:18 +0200

    Fixing final stylecop violation.

--- AutoEquals/AutoEquals.Lib/Properties/AssemblyInfo.cs
+++ AutoEquals/AutoEquals.Lib/Properties/AssemblyInfo.cs
@@ -1,8 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="AssemblyInfo.cs" company="AutoEquals">
-//   AutoEquals Library
-//   Licence: GNU GPL 2.0
-//   No warranty granted, use at your own risk.
+//   AutoEquals Library. Licence: GNU GPL 2.0. No warranty granted, use at your own risk.
 // </copyright>
 // <summary>
 //   AssemblyInfo.cs
